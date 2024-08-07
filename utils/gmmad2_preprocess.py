0c10b547a01c638978d7c706ea0910a6e6f71c0e
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-08-07 10:37:45 -0700

    export the .dat for MAGNN input

--- utils/gmmad2_preprocess.py
+++ utils/gmmad2_preprocess.py
@@ -1,83 +1,105 @@
+from biothings.utils.dataload import tabfile_feeder
 from data_preprocess_tools import (
     count_entity,
     load_data,
     map_disease_id2mondo,
     record_filter_attr1,
+    entity_filter_for_magnn,
+    export_data2dat
 )
 
 # Data preprocess for GMMAD2 database
 gmmad2_data_path = "../data/json/gmmad2_data.json"
 gmmad2_data = load_data(gmmad2_data_path)
-gmmad2_microbe_ct = count_entity(gmmad2_data, node="subject", attr="rank")
 
-#     gmmad2_disease_data = [rec for rec in gmmad2_data if "OrganismalEntityAsAModelOfDiseaseAssociation" in rec['association']['predicate']]
-#     gmmad2_microbes = [item['subject']['name'] for item in gmmad2_disease_data]
-#     gmmad2_diseases = [item['object']['name'] for item in gmmad2_disease_data]
-#
-#     print(f"count of gmmad2 microbes: {len(gmmad2_microbes)}")
-#     print(f"count of unique gmmad2 microbes: {len(set(gmmad2_microbes))}")
-#
-#     # print(f"count of microbial species with taxid: {len(microbe_species)}")
-#     # print(f"count of unique microbial species with taxid: {len(set(microbe_species))}")
-#     print(f"count of gmmad2 diseases: {len(gmmad2_diseases)}")
-#     print(f"count of unique gmmad2 diseases: {len(set(gmmad2_diseases))}")
+# filter out records with microbe-disease relationship only
+gmmad2_md_rec = record_filter_attr1(
+    data=gmmad2_data,
+    node="association",
+    attr="predicate",
+    include_vals="OrganismalEntityAsAModelOfDiseaseAssociation",
+    exclude_vals=None,
+)
 
-#     gmmad2_rank2count = [rec['subject'].get('rank') for rec in gmmad2_disease_data]
-#     rank_count = Counter(gmmad2_rank2count)
-#     print(f"count of taxon rank in GMMAD2: {rank_count}")
-#
-#     gmmad2df = [
-#         {
-#             'microbe': record['subject']['name'],
-#             'taxid': int(record['subject'].get('taxid')),
-#             'disease_name': record['object']['name'],
-#             'disease_id': record['object']['mesh'] if "mesh" in record['object'] else None
-#         }
-#         for record in gmmad2_data
-#         if "OrganismalEntityAsAModelOfDiseaseAssociation" in record['association']['predicate']
-#         if record['subject'].get('rank') == "species" or "strain"
-#     ]
-#
-# merged_md = disbiome2df + gmmad2df
-# print(merged_md)
-# print(f"total count of merged md set: {len(merged_md)}")
-#
-#
-# def remove_duplicate_microdis_sets(lst):
-#     seen = set()
-#     unique_list = []
-#     for d in lst:
-#         # Create a unique key based on taxid and disease_name
-#         md_sets = (d['taxid'], d['disease_name'])
-#         if md_sets not in seen:
-#             seen.add(md_sets)
-#             unique_list.append(d)
-#     return unique_list
-#
-#
-# unique_md_sets = remove_duplicate_microdis_sets(merged_md)
-# # print(unique_md_sets)
-# # print(len(unique_md_sets))
-#
-# merged_md_df = pd.DataFrame(unique_md_sets)
-#
-#
-# def convert_disease_id(value):
-#     if str(value).isdigit():
-#         return int(value)
-#     else:
-#         return str(value)
-#
-#
-# # Apply the custom function to the disease_id column
-# merged_md_df['disease_id'] = merged_md_df['disease_id'].apply(convert_disease_id)
-# merged_md_df = merged_md_df.dropna()
-# merged_md_df = merged_md_df.astype({
-#     'microbe': 'str',
-#     'taxid': 'Int64',
-#     'disease_name': 'str'
-# })
-#
-# merged_md_df.index += 1
-# merged_md_df.to_csv("../data/MAGNN_data/merged_microbe_disease.csv")
-# merged_md_df[["taxid", "disease_id"]].to_csv('../data/MAGNN_data/merged_microbe_disease.dat', sep='\t', index=False, header=False)
+# count rank types
+# except species rank, the other types need to use parent_taxid corresponding to species
+gmmad2_md_microbe_ct = count_entity(
+    gmmad2_md_rec, node="subject", attr="rank", split_char=None
+)
+# count disease records
+gmmad2_md_disease_ct = count_entity(
+    gmmad2_md_rec, node="object", attr="id", split_char=":"
+)
+
+# extract disease mesh id
+gmmad2_md_mesh = [
+    rec["object"]["mesh"] for rec in gmmad2_md_rec if "mesh" in rec["object"]
+]
+# convert mesh identifier to MONDO
+# mesh2mondo exp: {'D000086382': '0100096', ...} == {"mesh": "mondo"}
+mesh2mondo = map_disease_id2mondo(
+    query=gmmad2_md_mesh,
+    scope=["ctd.mesh"],
+    field="mondo",
+    unmapped_out_path="../data/manual/gmmad2_md_unmapped.csv",
+)
+# print(mesh2mondo)
+
+# load manually mapped disease data
+gmmad2_filled_disease_path = (
+    "../data/manual/gmmad2_disease_notfound_filled_mondo.txt"
+)
+
+# organize the disease mesh id and MONDO id to a dictionary
+# e.g., {'D016360': '0024388', ...} == {"mesh": "mondo"}
+mapped_gmmad2_disease = {}
+for line in tabfile_feeder(gmmad2_filled_disease_path, header=1):
+    mesh, mondo = line[0], line[1]
+    if mondo:
+        mapped_gmmad2_disease[mesh] = mondo
+    elif mesh:
+        mapped_gmmad2_disease[mesh] = mesh
+# print(mapped_gmmad2_disease)
+print("manually mapped disease count:", len(mapped_gmmad2_disease))
+
+# merge mesh2mondo + mapped mesh
+# merged output exp:
+# {'D037841': 'D037841', 'D010661': '0009861', ...} == {"mesh": "mesh", "mesh": "mondo", ...}
+gmmad2_complete_mapped_disease = mesh2mondo | mapped_gmmad2_disease
+# print(gmmad2_complete_mapped_disease)
+print(
+    "Merged mapped unique disease count:",
+    len(set(gmmad2_complete_mapped_disease)),
+)
+
+# TODO: how to make the code below cleaner?
+# need to add mondo to the filtered records that do not have mondo
+for rec in gmmad2_md_rec:
+    disease_mesh = rec["object"]["mesh"]
+    if disease_mesh in gmmad2_complete_mapped_disease:
+        if "D" not in gmmad2_complete_mapped_disease[disease_mesh]:
+            rec["object"]["id"] = f"MONDO:{gmmad2_complete_mapped_disease[disease_mesh]}"
+            rec["object"]["mondo"] = gmmad2_complete_mapped_disease[disease_mesh]
+# print(gmmad2_md_rec)
+
+# count the disease identifiers again after manual mapping (total:508,141)
+gmmad2_mapped_disease_ct = count_entity(
+    gmmad2_md_rec, node="object", attr="id", split_char=":"
+)
+
+# final record filter for MAGNN input with {taxid:mondo} only (508,141)
+gmmad2_data4magnn = entity_filter_for_magnn(gmmad2_md_rec,
+                                            node1="subject",
+                                            attr1="taxid",
+                                            val1=["strain", "subspecies", "biotype", "species group"],
+                                            node2="object",
+                                            attr2="id",
+                                            attr3="parent_taxid")
+# export the final filtered records to .dat file (495,936; 12,205 less)
+export_data2dat(
+    in_data=gmmad2_data4magnn,
+    col1="taxid",
+    col2="mondo",
+    out_path="../data/MAGNN_data/gmmad2_md_taxid_mondo.dat",
+    database="GMMAD2:Microbe-Disease",
+)
