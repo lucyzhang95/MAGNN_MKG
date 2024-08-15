feba035295a95e926ef0f2293b6b29d4fee049e2
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-08-15 10:42:37 -0700

    fix coding style

--- utils/record_filters.py
+++ utils/record_filters.py
@@ -1,8 +1,12 @@
 #
 def is_small_molecule_and_taxid(rec):
-    return rec.get("object", {}).get("type") == "biolink:SmallMolecule" and "taxid" in rec.get("subject", {})
+    return rec.get("object", {}).get(
+        "type"
+    ) == "biolink:SmallMolecule" and "taxid" in rec.get("subject", {})
 
 
 def is_small_molecule_and_gene(rec):
-    return rec.get("subject", {}).get("type") == "biolink:SmallMolecule" and rec.get("object", {}).get("type") == "biolink:Gene"
-
+    return (
+        rec.get("subject", {}).get("type") == "biolink:SmallMolecule"
+        and rec.get("object", {}).get("type") == "biolink:Gene"
+    )
