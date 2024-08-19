27e190561539f3e741b241cf6a4558589864d9bd
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-08-18 18:20:16 -0700

    add new filter criteria for gmmad2_md query need using record_id_filter

--- utils/record_filters.py
+++ utils/record_filters.py
@@ -10,3 +10,7 @@ def is_small_molecule_and_gene(rec):
         rec.get("subject", {}).get("type") == "biolink:SmallMolecule"
         and rec.get("object", {}).get("type") == "biolink:Gene"
     )
+
+
+def is_not_pubchem_cid(rec):
+    return ":" in rec["object"]["id"] and "PUBCHEM.COMPOUND" not in rec["object"]["id"]
