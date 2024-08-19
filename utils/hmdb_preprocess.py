f81d1208450783b2892f7364342a97133c2cef97
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-08-19 15:07:54 -0700

    add new fn

--- utils/hmdb_preprocess.py
+++ utils/hmdb_preprocess.py
@@ -1,19 +1,27 @@
-from data_preprocess_tools import load_data
 import pandas as pd
+from data_preprocess_tools import (
+    load_data,
+    load_ncbi_taxdump,
+    map_microbe_name2taxid,
+)
 
+# Load HMDB data
 data_path = "../data/json/hmdb_microbe_metabolites.json"
 data = load_data(data_path)
+# microbial names to map (118 unique)
 micro_names = [
-    taxon.get("scientific_name")
+    taxon.get("scientific_name").strip()
     for rec in data
     if "associated_microbes" in rec
     for taxon in rec.get("associated_microbes")
     if "taxid" not in taxon
 ]
 
-micro_names.sort(key=lambda x: x[0])
-micro_names_df = pd.DataFrame(micro_names, columns=["microbe_name"])
-name_out = "../data/manual/hmdb_microbe_names_notfound.csv"
-micro_names_df.to_csv(name_out, sep="\t", header=True, index=False)
-
 # TODO: use NCBI .dmp file to map the out-dated microbial names to taxid
+taxdump_path = "taxdump.tar.gz"
+mapping_src = load_ncbi_taxdump(taxdump_path)
+mapped_microbe_names = map_microbe_name2taxid(
+    mapping_src=mapping_src,
+    microbe_names=micro_names,
+    unmapped_out_path="../data/manual/hmdb_microbe_names_notfound.csv",
+)
