630faac4af7b6e303144f1e10af3651661587c53
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-08-20 22:45:26 -0700

    update with relative import

--- analysis/analysis_utils/__init__.py
+++ analysis/analysis_utils/__init__.py
@@ -1,4 +1,4 @@
-from analysis_tools import load_data, merge_df, filter_entity, plot_entity_distribution
+from .analysis_tools import load_data, merge_df, filter_entity, plot_entity_distribution
 
 __all__ = [
     'load_data',
