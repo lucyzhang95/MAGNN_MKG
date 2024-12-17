3d15abbc7ccdf420f768cdbc721b577d2e798be8
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-12-16 18:14:51 -0800

    simplify the codes for our case

--- MAGNN/MAGNN_utils/data_loader.py
+++ MAGNN/MAGNN_utils/data_loader.py
@@ -1,68 +1,64 @@
-import pickle
+import os
 
 import numpy as np
 import scipy
 
+from MAGNN.MAGNN_utils.preprocess import load_compressed_pickle
+
 
 def load_preprocessed_data(prefix="data/preprocessed/"):
-    in_file = open(prefix + "/0/0-1-0.adjlist", "r")
-    adjlist00 = [line.strip() for line in in_file]
-    adjlist00 = adjlist00
-    in_file.close()
-    in_file = open(prefix + "/0/0-1-2-1-0.adjlist", "r")
-    adjlist01 = [line.strip() for line in in_file]
-    adjlist01 = adjlist01
-    in_file.close()
-    in_file = open(prefix + "/0/0-0.adjlist", "r")
-    adjlist02 = [line.strip() for line in in_file]
-    adjlist02 = adjlist02
-    in_file.close()
-    in_file = open(prefix + "/1/1-0-1.adjlist", "r")
-    adjlist10 = [line.strip() for line in in_file]
-    adjlist10 = adjlist10
-    in_file.close()
-    in_file = open(prefix + "/1/1-2-1.adjlist", "r")
-    adjlist11 = [line.strip() for line in in_file]
-    adjlist11 = adjlist11
-    in_file.close()
-    in_file = open(prefix + "/1/1-0-0-1.adjlist", "r")
-    adjlist12 = [line.strip() for line in in_file]
-    adjlist12 = adjlist12
-    in_file.close()
+    def load_adjlists(file_names, folder):
+        adjlists = []
+        for file_name in file_names:
+            file_path = os.path.join(prefix, folder, f"{file_name}_adjlist.gz")
+            adjlist = load_compressed_pickle(file_path)
+            adjlist = [
+                adjlist_idx
+                for _, value in adjlist.items()
+                if value
+                for adjlist_idx in value
+            ]
+            adjlists.append(adjlist)
+        return adjlists
 
-    in_file = open(prefix + "/0/0-1-0_idx.pickle", "rb")
-    idx00 = pickle.load(in_file)
-    in_file.close()
-    in_file = open(prefix + "/0/0-1-2-1-0_idx.pickle", "rb")
-    idx01 = pickle.load(in_file)
-    in_file.close()
-    in_file = open(prefix + "/0/0-0_idx.pickle", "rb")
-    idx02 = pickle.load(in_file)
-    in_file.close()
-    in_file = open(prefix + "/1/1-0-1_idx.pickle", "rb")
-    idx10 = pickle.load(in_file)
-    in_file.close()
-    in_file = open(prefix + "/1/1-2-1_idx.pickle", "rb")
-    idx11 = pickle.load(in_file)
-    in_file.close()
-    in_file = open(prefix + "/1/1-0-0-1_idx.pickle", "rb")
-    idx12 = pickle.load(in_file)
-    in_file.close()
+    def load_indices(file_names, folder):
+        indices = []
+        for file_name in file_names:
+            file_path = os.path.join(prefix, folder, f"{file_name}_idx.gz")
+            idx = load_compressed_pickle(file_path)
+            indices.append(idx)
+        return indices
+
+    files_0 = ["0-1-0", "0-1-2-1-0", "0-2-0", "0-2-1-2-0"]
+    files_1 = ["1-0-1", "1-0-2-0-1", "1-2-0-2-1", "1-2-1"]
+    files_2 = ["2-0-2", "2-0-1-0-2", "2-1-0-1-2", "2-1-2"]
+
+    adjlists_0 = load_adjlists(files_0, "0")
+    adjlists_1 = load_adjlists(files_1, "1")
+    adjlists_2 = load_adjlists(files_2, "2")
+
+    idxlists_0 = load_indices(files_0, "0")
+    idxlists_1 = load_indices(files_1, "1")
+    idxlists_2 = load_indices(files_2, "2")
 
     adjM = scipy.sparse.load_npz(prefix + "/adjM.npz")
     type_mask = np.load(prefix + "/node_types.npy")
-    train_val_test_pos_user_artist = np.load(
-        prefix + "/train_val_test_pos_user_artist.npz"
+    train_val_test_pos_microbe_disease = np.load(
+        prefix
+        + "/microbe_disease_neg_pos_processed"
+        + "/train_val_test_pos_microbe_disease.npz"
     )
-    train_val_test_neg_user_artist = np.load(
-        prefix + "/train_val_test_neg_user_artist.npz"
+    train_val_test_neg_microbe_disease = np.load(
+        prefix
+        + "/microbe_disease_neg_pos_processed"
+        + "/train_val_test_neg_microbe_disease.npz"
     )
 
     return (
-        [[adjlist00, adjlist01, adjlist02], [adjlist10, adjlist11, adjlist12]],
-        [[idx00, idx01, idx02], [idx10, idx11, idx12]],
+        [adjlists_0, adjlists_1, adjlists_2],
+        [idxlists_0, idxlists_1, idxlists_2],
         adjM,
         type_mask,
-        train_val_test_pos_user_artist,
-        train_val_test_neg_user_artist,
+        train_val_test_pos_microbe_disease,
+        train_val_test_neg_microbe_disease,
     )
