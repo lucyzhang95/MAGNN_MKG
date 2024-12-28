fb28908d7ac1ec8a79560e4ac05f605ef28ed958
Lucy Zhang <bailin.zhang1116@gmail.com>
2024-12-28 00:52:40 -0800

    change lr to 0.001

--- MAGNN/run_sampled_MAGNN.py
+++ MAGNN/run_sampled_MAGNN.py
@@ -16,7 +16,7 @@ from model import MAGNN_lp_2metapaths_layer
 # Params
 num_ntype = 3  # microbe, disease, metabolite = 3
 dropout_rate = 0.5
-lr = 0.005
+lr = 0.001
 weight_decay = 0.001
 
 # [0, 1, 0]: ([0, 1] is 0 and [1, 0] is 1 = [0, 1])
