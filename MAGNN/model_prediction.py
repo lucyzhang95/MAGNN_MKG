eaf94bcc2feb38c622ffe0cd6333dbd2b7983e15
Lucy Zhang <bailin.zhang1116@gmail.com>
2025-01-17 02:14:44 -0800

    change postfix

--- MAGNN/model_prediction.py
+++ MAGNN/model_prediction.py
@@ -650,7 +650,7 @@ def train():
 
     config = wandb.config
 
-    save_postfix = f"MID_prediction_model_{args.save_postfix}_epoch{config.num_epochs}"
+    save_postfix = f"MID_prediction_model_{args.save_postfix}_epoch{config.num_epochs}_drp{config.dropout_rate}"
 
     run_model(
         config.feats_type,
